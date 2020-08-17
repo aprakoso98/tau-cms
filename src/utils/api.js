447a096e25e0ba612fe8f25532cc828399d6c956
Bambang Adhitya <bambang@kiosawan.com>
2020-08-17 22:40:44 +0700

    ss

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://api-cms.kadetech.tech/"
+const BASE_URL = "https://api-cms.kadetech.tech"
 // const BASE_URL = "http://127.0.0.1/tau-api"
 
 const API = BASE_URL + "/api.php"
