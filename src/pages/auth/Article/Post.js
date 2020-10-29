65ad159137fa9ff5092a8ee33c47be293f7ec973
aprakoso98 <adhyt.scott@gmail.com>
2020-10-29 17:16:11 +0700

    Udah kali ya?

--- src/pages/auth/Article/Post.js
+++ src/pages/auth/Article/Post.js
@@ -20,7 +20,9 @@ const PostArticle = ({ location: { state: urlEdit } = {} }) => {
 	const Web = useSelector(state => state.Web)
 	const history = useHistory()
 	const dispatch = useDispatch()
-	const [article, setState] = useState({})
+	const [article, setState] = useState({
+		hideTanggal: 0
+	})
 	const [render, setRender] = useState(true)
 	const setArticle = v => setState({ ...winArticle, ...v })
 	const onChange = ({ target: { id, value } }) => setArticle({ [id]: value })
@@ -47,8 +49,8 @@ const PostArticle = ({ location: { state: urlEdit } = {} }) => {
 				if (status) {
 					setTimeout(() => setRender(true), 10)
 					setRender(false)
-					winArticle = { artikel: '' }
-					setState({ artikel: '' })
+					winArticle = { artikel: '', url: "".uuid() }
+					setState({ artikel: '', url: "".uuid() })
 				}
 			}
 		}
@@ -86,7 +88,7 @@ const PostArticle = ({ location: { state: urlEdit } = {} }) => {
 				toBase64
 				className="flex b-1-dark mr-3"
 				imgClass="h-30 w-auto"
-				src={!article.foto ? imgThumb : article.foto.length > 50 ? article.foto : FILE_PATH + article.foto}
+				src={!article.foto ? imgThumb : article.foto.length > 100 ? article.foto : FILE_PATH + article.foto}
 				accept="image/*"
 				onChange={({ file: foto }) => setArticle({ foto })}
 			/>
@@ -95,15 +97,22 @@ const PostArticle = ({ location: { state: urlEdit } = {} }) => {
 				<div className="mt-2 mb-2">Permalink : <a rel="noopener noreferrer" target="_blank" className="bb-1-link" href={"https://rev.tau.ac.id/" + article.url}>{"https://rev.tau.ac.id/" + article.url}</a></div>
 			</View>
 		</View>
-		<View direction="row" className="mb-1">
+		<View direction="row" className="ai-c mb-1">
 			<Input placeholder="Kustom Url" onBlur={() => {
 				if (article.url) {
 					const url = article.url.replace(/\W/g, "-")
 					setArticle({ url })
 				}
 			}} className="flex-1" id="url" onChange={onChange} value={article.url} />
+			<Input placeholder="Pembuat" id="pembuat" className="ml-3 flex-1" onChange={onChange} value={article.pembuat} />
 			<Input placeholder="Tanggal" type="datetime-local" id="tgl" className="flex-1 mr-3 ml-3" onChange={onChange} value={tglValue()} />
-			<Input placeholder="Pembuat" id="pembuat" className="flex-1" onChange={onChange} value={article.pembuat} />
+			<label title="Perlihatkan atau sembunyikan tanggal pada artikel" htmlFor="hideTanggal">
+				<input checked={['1', 1].includes(article.hideTanggal)} type="checkbox" className="mr-1" onChange={e => {
+					const value = e.target.checked ? 1 : 0
+					onChange({ target: { id: 'hideTanggal', value } })
+				}} id="hideTanggal" />
+				Hide Tanggal
+			</label>
 		</View>
 		<Input className="mb-1" placeholder="Deskripsi" id="deskripsi" onChange={onChange} value={article.deskripsi} />
 		<JoditEditor
